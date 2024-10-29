45e1e31eb5bad12e09e0303cf35af7b2da784316
SaeedKarimian860 <SGK12190@gmail.com>
2024-10-28 23:28:11 -0400

    callbacks

---
+++ app.js
@@ -0,0 +1,75 @@
+const btn = document.querySelector("button");
+
+// setTimeout(() => {
+//   btn.style.transform = `translateX(100px)`;
+//   setTimeout(() => {
+//     btn.style.transform = `translateX(200px)`;
+//     setTimeout(() => {
+//       btn.style.transform = `translateX(300px)`;
+//       setTimeout(() => {
+//         btn.style.transform = `translateX(400px)`;
+//         setTimeout(() => {
+//           btn.style.transform = `translateX(500px)`;
+//         }, 1000);
+//       }, 1000);
+//     }, 1000);
+//   }, 1000);
+// }, 1000);
+
+const moveX = (element, amount, delay, onSuccess, onFailure) => {
+  const bodyBoundary = document.body.clientWidth;
+  const elRight = element.getBoundingClientRect().right;
+  const currLeft = element.getBoundingClientRect().left;
+  if (elRight + amount > bodyBoundary) {
+    onFailure();
+  } else {
+    setTimeout(() => {
+      element.style.transform = `translateX(${currLeft + amount}px)`;
+      onSuccess();
+    }, delay);
+  }
+};
+// moveX (btn, 100, 1000, () => {
+//   moveX (btn, 100, 1000, () => {
+//     moveX (btn, 100, 1000, () => {
+//       moveX (btn, 100, 1000, () => {
+//         moveX (btn, 1000, 1000);
+//       });
+//     });
+//   });
+// });
+
+moveX(
+  btn,
+  100,
+  1000,
+  () => {
+    //Success
+    moveX(
+      btn,
+      400,
+      1000,
+      () => {
+        moveX(
+          btn,
+          700,
+          1000,
+          () => {
+            console.log("REALLY, WE STILL HAVE SCREEN LEFT?");
+          },
+          () => {
+            alert("CANNOT MOVE FURTHER!");
+          }
+        );
+      },
+      () => {
+        //fail
+        alert("CANNOT MOVE FURTHER!");
+      }
+    );
+  },
+  () => {
+    //fail
+    alert("CANNOT MOVE FURTHER!");
+  }
+);
