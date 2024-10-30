07f2cf4d31d5c92bb19c94c259ff202c63517ca6
SaeedKarimian860 <SGK12190@gmail.com>
2024-10-30 00:07:48 -0400

    Callbacks and Promises

--- app.js
+++ app.js
@@ -1,4 +1,47 @@
 const btn = document.querySelector("button");
+const moveX = (element, amount, delay) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const bodyBoundary = document.body.clientWidth;
+      const elRight = element.getBoundingClientRect().right;
+      const currLeft = element.getBoundingClientRect().left;
+      if (elRight + amount > bodyBoundary) {
+        reject({ bodyBoundary, elRight, amount });
+      } else {
+        element.style.transform = `translateX(${currLeft + amount}px)`;
+        resolve();
+      }
+    }, delay);
+  });
+};
+
+// moveX(btn, 300, 1000).then(() => {
+//   return moveX(btn, 300, 1000)
+// })
+// .then (() => {
+//   return moveX(btn, 300, 1000);
+// })
+// .then (() => {
+//   return moveX(btn, 300, 1000);
+// })
+// .then(() => {
+//   console.log('DONE WITH BOTH!');
+// })
+// .catch(() => {
+//   console.log('OUT OF SPACE, CANNOT MOVE!');
+// });
+
+//could be refactored and written like this
+moveX(btn, 300, 1000)
+  .then(() => moveX(btn, 300, 1000))
+  .then(() => moveX(btn, 300, 1000))
+  .then(() => moveX(btn, 300, 1000))
+  .then(() => moveX(btn, 300, 1000))
+  .then(() => moveX(btn, 300, 1000))
+  .catch(({ bodyBoundary, elRight, amount }) => {
+    console.log(`Body is ${bodyBoundary}px wide`);
+    console.log(`Element is at ${elRight}px, ${amount}px is too large!`);
+  });
 
 // setTimeout(() => {
 //   btn.style.transform = `translateX(100px)`;
@@ -16,19 +59,19 @@ const btn = document.querySelector("button");
 //   }, 1000);
 // }, 1000);
 
-const moveX = (element, amount, delay, onSuccess, onFailure) => {
-  const bodyBoundary = document.body.clientWidth;
-  const elRight = element.getBoundingClientRect().right;
-  const currLeft = element.getBoundingClientRect().left;
-  if (elRight + amount > bodyBoundary) {
-    onFailure();
-  } else {
-    setTimeout(() => {
-      element.style.transform = `translateX(${currLeft + amount}px)`;
-      onSuccess();
-    }, delay);
-  }
-};
+// const moveX = (element, amount, delay, onSuccess, onFailure) => {
+//   const bodyBoundary = document.body.clientWidth;
+//   const elRight = element.getBoundingClientRect().right;
+//   const currLeft = element.getBoundingClientRect().left;
+//   if (elRight + amount > bodyBoundary) {
+//     onFailure();
+//   } else {
+//     setTimeout(() => {
+//       element.style.transform = `translateX(${currLeft + amount}px)`;
+//       onSuccess();
+//     }, delay);
+//   }
+// };
 // moveX (btn, 100, 1000, () => {
 //   moveX (btn, 100, 1000, () => {
 //     moveX (btn, 100, 1000, () => {
@@ -39,37 +82,37 @@ const moveX = (element, amount, delay, onSuccess, onFailure) => {
 //   });
 // });
 
-moveX(
-  btn,
-  100,
-  1000,
-  () => {
-    //Success
-    moveX(
-      btn,
-      400,
-      1000,
-      () => {
-        moveX(
-          btn,
-          700,
-          1000,
-          () => {
-            console.log("REALLY, WE STILL HAVE SCREEN LEFT?");
-          },
-          () => {
-            alert("CANNOT MOVE FURTHER!");
-          }
-        );
-      },
-      () => {
-        //fail
-        alert("CANNOT MOVE FURTHER!");
-      }
-    );
-  },
-  () => {
-    //fail
-    alert("CANNOT MOVE FURTHER!");
-  }
-);
+// moveX(
+//   btn,
+//   100,
+//   1000,
+//   () => {
+//     //Success
+//     moveX(
+//       btn,
+//       400,
+//       1000,
+//       () => {
+//         moveX(
+//           btn,
+//           700,
+//           1000,
+//           () => {
+//             console.log("REALLY, WE STILL HAVE SCREEN LEFT?");
+//           },
+//           () => {
+//             alert("CANNOT MOVE FURTHER!");
+//           }
+//         );
+//       },
+//       () => {
+//         //fail
+//         alert("CANNOT MOVE FURTHER!");
+//       }
+//     );
+//   },
+//   () => {
+//     //fail
+//     alert("CANNOT MOVE FURTHER!");
+//   }
+// );
