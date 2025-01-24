b64be5ba8637227708deceb506bddc99fa8f870f
SaeedKarimian860 <SGK12190@gmail.com>
2025-01-24 08:09:37 -0500

    CallStack Practice

--- Callstack/app.js
+++ Callstack/app.js
@@ -1,7 +1,13 @@
-const multiply = (x, y) => x * y;
+// const multiply = (x, y) => x * y;
 
-const square = x => multiply(x, x);
+// const square = x => multiply(x, x);
 
-const isRightTriangle = (a, b, c) => (
-    square(a) + square(b) === square(c)
-)
+// const isRightTriangle = (a, b, c) => (
+//     square(a) + square(b) === square(c)
+// )
+
+console.log("Sending request to server")
+setTimeout(() => {
+    console.log("Here is your data from the server...")
+}, 3000)
+console.log("I am at the end of the file!")
