b50be14ab4f158fb96dcce5cf12299734340d6e2
SaeedKarimian860 <SGK12190@gmail.com>
2025-01-25 18:00:00 -0500

    Async Functions

--- AsyncFunctions/app.js
+++ AsyncFunctions/app.js
@@ -32,3 +32,48 @@ login('BigO') //if you pass in the correct password it will log you in! otherwis
     console.log("Error!")
     console.log(err)
 })
+
+
+
+
+
+
+
+
+const delayedColorChange = (color, delay) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            document.body.style.backgroundColor = color;
+            resolve();
+        }, delay)
+    })
+}
+
+
+// delayedColorChange('red', 1000)
+// .then(() => delayedColorChange('orange', 1000))
+// .then(() => delayedColorChange('yellow', 1000))
+// .then(() => delayedColorChange('green', 1000))
+// .then(() => delayedColorChange('blue', 1000))
+// .then(() => delayedColorChange('indigo', 1000))
+// .then(() => delayedColorChange('violet', 1000))
+
+
+async function rainbow() {
+    await delayedColorChange('red', 1000)
+    await delayedColorChange('orange', 1000)
+    await delayedColorChange('yellow', 1000)
+    await delayedColorChange('green', 1000)
+    await delayedColorChange('indigo', 1000)
+    await delayedColorChange('violet', 1000)
+    return "All Done!"
+}
+
+// rainbow().then(() => console.log("End of Rainbow!"))
+
+async function printRainbow() {
+    await rainbow();
+    console.log("End of Rainbow!")
+}
+
+printRainbow();
