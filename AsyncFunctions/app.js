1e29882c8c8b9012055a83004bad65f60f3d8cb5
SaeedKarimian860 <SGK12190@gmail.com>
2025-01-25 17:37:43 -0500

    Async Functions

---
+++ AsyncFunctions/app.js
@@ -0,0 +1,17 @@
+// async function hello() {
+// }
+
+// const sing = async () => {
+//     throw "Oh No, Problem!"
+//     return 'La La La La'
+// }
+
+// sing()
+// .then((data) => {
+//     console.log('Promise resolved with:', data)
+// })
+// .catch((err) => {
+//     console.log('Promise rejected!')
+//     console.log(err)
+// })
+
