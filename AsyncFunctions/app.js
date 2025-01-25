142e0c463eb8aa6107f972d0534786788e30d65f
SaeedKarimian860 <SGK12190@gmail.com>
2025-01-25 17:47:04 -0500

    Async Functions

--- AsyncFunctions/app.js
+++ AsyncFunctions/app.js
@@ -15,3 +15,20 @@
 //     console.log(err)
 // })
 
+
+
+const login = async (username, password) => {
+    if (!username || !password) throw 'Missing Credentials'
+    if (password === 'tinykittens') return 'Welcome!'
+    throw 'Invalid Password'
+}
+
+login('BigO') //if you pass in the correct password it will log you in! otherwise it will throw the correct errors
+.then(msg => {
+    console.log("Logged In")
+    console.log(msg)
+})
+.catch(err => {
+    console.log("Error!")
+    console.log(err)
+})
