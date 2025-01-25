3045a6ab289bed738044e8411fc097b10504c74b
SaeedKarimian860 <SGK12190@gmail.com>
2025-01-24 23:45:22 -0500

    Callback Hell

--- Callback_Hell/app.js
+++ Callback_Hell/app.js
@@ -1,15 +1,20 @@
-setTimeout(() => {
-    document.body.style.backgroundColor = 'red';
-    setTimeout(() => {
-        document.body.style.backgroundColor = 'orange';
-        setTimeout(() => {
-            document.body.style.backgroundColor = 'yellow';
-            setTimeout(() => {
-                document.body.style.backgroundColor = 'green';
-                setTimeout(() => {
-                    document.body.style.backgroundColor = 'blue';
-                }, 1000)
-            }, 1000)
-        }, 1000)
-    }, 1000)
-}, 1000)
+// setTimeout(() => {
+//     document.body.style.backgroundColor = 'red';
+//     setTimeout(() => {
+//         document.body.style.backgroundColor = 'orange';
+//         setTimeout(() => {
+//             document.body.style.backgroundColor = 'yellow';
+//             setTimeout(() => {
+//                 document.body.style.backgroundColor = 'green';
+//                 setTimeout(() => {
+//                     document.body.style.backgroundColor = 'blue';
+//                 }, 1000)
+//             }, 1000)
+//         }, 1000)
+//     }, 1000)
+// }, 1000)
+
+
+const delayedColorChange = (newColor, delay) => {
+    setTimeout
+}
