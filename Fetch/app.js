a19b337a924f22d7ecb51a0222851e6c12140dda
SaeedKarimian860 <SGK12190@gmail.com>
2025-01-25 23:03:00 -0500

    Fetch Requests

--- Fetch/app.js
+++ Fetch/app.js
@@ -1,7 +1,47 @@
 fetch("https://swapi.tech/api/people/1/")
 .then(res => {
     console.log("Resolved", res);
+    return res.json()
+})
+.then((data) => {
+    console.log(data.result.properties);
 })
 .catch((e) => {
     console.log("ERROR", e);
 });
+
+// fetch("https://swapi.tech/api/people/1/")
+//   .then((res) => {
+//     console.log("RESOLVED!", res);
+//     return res.json();
+//   })
+//   .then((data) => {
+//     console.log(data.result.properties);
+//     return fetch("https://swapi.tech/api/people/2/");
+//   })
+//   .then((res) => {
+//     console.log("SECOND REQUEST RESOLVED!!!");
+//     return res.json();
+//   })
+//   .then((data) => {
+//     console.log(data.result.properties);
+//   })
+//   .catch((e) => {
+//     console.log("ERROR!", e);
+//   });
+
+
+const loadStarWarsPeople = async () => {
+    try {
+        const res = await fetch("https://swapi.tech/api/people/1/");
+        const data = await res.json();
+        console.log(data.result.properties);
+        const res2 = await fetch("https://swapi.tech/api/people/2/");
+        const data2 = await res2.json();
+        console.log(data.result.properties);
+    } catch (e) {
+        console.log("ERROR", e);
+    }
+};
+
+loadStarWarsPeople();
